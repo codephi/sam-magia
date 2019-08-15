4a7b7daf8f79fb74a7948bda4948a2c8ca9faa87
Philippe Assis <assis@philippeassis.com>
2019-08-15 18:18:54 -0300

    tree

--- hander.js
+++ hander.js
@@ -1,19 +1,22 @@
 #!/bin/node
 const path = require('path')
+const fs = require('fs')
 const exec = require('child_process').execSync
 const command = process.argv[2]
 
 const folders = []
 
-exec('git --no-pager log --name-only --max-count 1 --oneline | sed -n \'1!p\'')
+exec('git --no-pager log --name-status --max-count 1 --oneline | sed -n \'1!p\'')
     .toString()
     .split('\n')
     .filter(item => item)
-    .map(item => {
-        const dir = path.dirname(item)
-        if(!~folders.indexOf(dir)){
-            folders.push(dir)
-        }
+    .forEach(item => {
+        item.split('\t').forEach(file => {
+            const dir = path.dirname(file)
+            if(~dir.indexOf('lambdas/') & fs.existsSync(dir) && folders.indexOf(dir) === -1){
+                folders.push(command.replace('$file', dir))
+            }
+        })
     })
 
 
