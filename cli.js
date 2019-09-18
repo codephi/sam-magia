5046cda7f1eff113e8739b1ae12a5dcd5053cc81
Philippe Assis <assis@philippeassis.com>
2019-09-18 11:06:26 -0300

    Fix

--- cli.js
+++ cli.js
@@ -5,64 +5,56 @@ const getTemplates = require('./libs/getTemplates')
 const fs = require('fs')
 
 const samCommands = []
-let rootPath = process.cwd()
+const rootPath = process.argv[2] ? path.resolve(process.argv[2]) : process.cwd()
+const deployFile = process.argv[3] || path.resolve(rootPath, 'deploy.sh')
 
 try {
-    if(process.argv[2]){
-        rootPath = path.resolve(process.argv[2])
-
-        if(process.argv[3]){
-            samCommands.push(fs.readFileSync(process.argv[3]).toString())
-        } else {
-            samCommands.push(fs.readFileSync(path.resolve(rootPath, 'deploy.sh')).toString())
-        }
-    } else {
-        samCommands.push(fs.readFileSync(path.resolve(rootPath, 'deploy.sh')).toString())
-    }
-} catch(err) {
-    return console.error('Deploy file not found.')
+  samCommands.push(fs.readFileSync(deployFile).toString())
+} catch (err) {
+  console.error('Deploy file not found.')
+  process.exit()
 }
 
 const templates = getTemplates(rootPath)
 
 console.log('\nHANDLER COMMANDS...')
 
-function packageOutputPath(key){
-    return  path.resolve(rootPath, '.packages', `${Date.now()}-${key}.yml` )
+function packageOutputPath (key) {
+  return path.resolve(rootPath, '.packages', `${Date.now()}-${key}.yml`)
 }
 
 const commandsRun = templates.map(template => {
-    const commands = []
+  const commands = []
 
-    for(let key in samCommands){
-        const packageOutput = packageOutputPath(key)
-        const comm = samCommands[key].replace(/\$templateBasePath/g , template[0])
-            .replace(/\$templateFilename/g, template[2])
-            .replace(/\$packagePath/g , packageOutput)
+  for (const key in samCommands) {
+    const packageOutput = packageOutputPath(key)
+    const comm = samCommands[key].replace(/\$templateBasePath/g, template[0])
+      .replace(/\$templateFilename/g, template[2])
+      .replace(/\$packagePath/g, packageOutput)
 
-        commands.push(comm)
-    }
+    commands.push(comm)
+  }
 
-    return commands
+  return commands
 })
 
 console.log('\n---=INIT DEPLOY=---')
 
 try {
-    commandsRun.forEach(command => {
-        try {
-            for(let i = 0; i < command.length; i++){
-                console.log('\n-> Exec command', 1)
-                console.log(`\n${command[i]}`)
-                const stout = exec(command[i])
-                console.log(stout.toString())
-            }
-        } catch (err){
-            throw new Error(err.message.toString())
-        }
-    })
+  commandsRun.forEach(command => {
+    try {
+      for (let i = 0; i < command.length; i++) {
+        console.log('\n-> Exec command', 1)
+        console.log(`\n${command[i]}`)
+        const stout = exec(command[i])
+        console.log(stout.toString())
+      }
+    } catch (err) {
+      throw new Error(err.message.toString())
+    }
+  })
 
-    console.log('\nFINISH!!!')
-} catch (err){
-    console.error(err.message)
+  console.log('\nFINISH!!!')
+} catch (err) {
+  console.error(err.message)
 }
--- libs/getTemplates.js
+++ libs/getTemplates.js
@@ -3,79 +3,78 @@ const fs = require('fs')
 const exec = require('child_process').execSync
 const gitignore = require('./gitignore')
 
-function findTemplate(dir, ignore){
-    const dirSplit = dir.split('/')
+function findTemplate (dir, ignore) {
+  const dirSplit = dir.split('/')
 
-    for(let i = dirSplit.length; i > 0; i--){
-        const dirJoin = dirSplit.splice(0,i).join('/')
+  for (let i = dirSplit.length; i > 0; i--) {
+    const dirJoin = dirSplit.splice(0, i).join('/')
 
-        if(ignore.absolute.find(item => dirJoin.indexOf(item) === 0) ||
-            ignore.relative.find(item => dirJoin.indexOf(`/${item}`) > -1)){
-            continue
-        }
-
-        const pathSplit = [
-            path.resolve(dirJoin, 'template.yml'),
-            path.resolve(dirJoin, 'template.yaml'),
-            path.resolve(dirJoin, 'template.json')
-        ]
-
-        const template = pathSplit.find(item => fs.existsSync(item))
-
-        if(template){
-            return [path.resolve(dirJoin), template, template.split('/').slice(-1)]
-        }
+    if (ignore.absolute.find(item => dirJoin.indexOf(item) === 0) ||
+            ignore.relative.find(item => dirJoin.indexOf(`/${item}`) > -1)) {
+      continue
     }
+
+    const pathSplit = [
+      path.resolve(dirJoin, 'template.yml'),
+      path.resolve(dirJoin, 'template.yaml'),
+      path.resolve(dirJoin, 'template.json')
+    ]
+
+    const template = pathSplit.find(item => fs.existsSync(item))
+
+    if (template) {
+      return [path.resolve(dirJoin), template, template.split('/').slice(-1)]
+    }
+  }
 }
 
-
 module.exports = (rootPath) => {
-    const files = []
-    const templatesDir = []
+  const templatesDir = []
+  const files = []
 
-    exec(`cd ${rootPath} && git --no-pager log --name-status --max-count 1 --oneline | sed -n \'1!p\'`)
-        .toString()
-        .split('\n')
-        .filter(item => item)
-        .forEach(item => {
-            item.split('\t').slice(1).forEach(file => {
-                if(files.indexOf(file) === -1){
-                    files.push(file)
-                }
-            })
-        })
+  const current = exec(`cd ${rootPath} && git reflog  --no-abbrev | grep -A1 pull | head -2 | awk '{print $1}' | sed -n 1p`).toString().trim()
+  const last = exec(`cd ${rootPath} && git reflog  --no-abbrev | grep -A1 pull | head -2 | awk '{print $1}' | sed -n 2p`).toString().trim()
+  const list = exec(`cd ${rootPath} && git diff ${last}..${current} --raw | cat | awk '{print $6}'`).toString()
+    .trim()
+    .split('\n')
 
-    console.log('Modified files:')
-    console.log(' - ' + files.join('\n - '))
-
-    const ignore = {relative: [], absolute: []}
-
-    try {
-        const gitIgnorePath = path.resolve(rootPath, '.gitignore')
-        const gitIgnoreFile = fs.readFileSync(gitIgnorePath).toString()
-
-        gitignore.parse(gitIgnoreFile).patterns.forEach(item => {
-            if(item.indexOf('/') === 0){
-                ignore.absolute.push(item)
-            } else {
-                ignore.relative.push(item)
-            }
-        })
-    } catch (err){
-        console.error('.gitignore not found.')
+  list.forEach(file => {
+    if (files.indexOf(file) === -1) {
+      files.push(file)
     }
+  })
 
-    console.log('\nFound template directories:')
+  console.log('Modified files:')
+  console.log(' - ' + files.join('\n - '))
 
-    files.map(file => {
-        const dir = path.dirname(file)
-        const template = findTemplate(dir, ignore)
+  const ignore = { relative: [], absolute: [] }
 
-        if(template && templatesDir.indexOf(template) === -1){
-            templatesDir.push(template)
-            console.log(' - ' + template[1])
-        }
+  try {
+    const gitIgnorePath = path.resolve(rootPath, '.gitignore')
+    const gitIgnoreFile = fs.readFileSync(gitIgnorePath).toString()
+
+    gitignore.parse(gitIgnoreFile).patterns.forEach(item => {
+      if (item.indexOf('/') === 0) {
+        ignore.absolute.push(item)
+      } else {
+        ignore.relative.push(item)
+      }
     })
+  } catch (err) {
+    console.error('.gitignore not found.')
+  }
 
-    return templatesDir
+  console.log('\nFound template directories:')
+
+  files.map(file => {
+    const dir = path.dirname(file)
+    const template = findTemplate(dir, ignore)
+
+    if (template && templatesDir.indexOf(template) === -1) {
+      templatesDir.push(template)
+      console.log(' - ' + template[1])
+    }
+  })
+
+  return templatesDir
 }
